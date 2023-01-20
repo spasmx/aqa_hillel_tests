3cb55a0ae9f17dbc86c548e0af1b39d5cc907837
spasmx <maksimlushchan@gmail.com>
2023-01-20 17:15:45 +0200

    HW 17 tex-box

--- selenium_tests_demoqa/text_box/conftest.py
+++ selenium_tests_demoqa/text_box/conftest.py
@@ -6,8 +6,8 @@ from selenium.webdriver.chrome.options import Options as chrome_options
 @pytest.fixture
 def get_chrome_options():
     options = chrome_options()
-    options.add_argument('chrome') #Use --headless if you don't need browser UI
-    options.add_argument('--window-size=1080,1080')
+    #options.add_argument('--headless') #Use --headless if you don't need browser UI
+    options.add_argument('--window-size=477,653')
     return options
 
 
