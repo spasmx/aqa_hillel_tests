ba0c4a3ec2c9a8ea08b6bafdb09ffcaa29bb5878
spasmx <maksimlushchan@gmail.com>
2023-02-01 17:29:33 +0200

    HW 17 demoqa.com/text-box/

---
+++ selenium_tests_demoqa/dynamic-properties/conftest.py
@@ -0,0 +1,43 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chrome_options
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture
+def get_chrome_options():
+    options = chrome_options()
+    #options.add_argument('--headless') #Use --headless if you don't need browser UI
+    options.add_argument('--window-size=1080,1080')
+    return options
+
+
+@pytest.fixture
+def get_webdriver(get_chrome_options):
+    options = get_chrome_options
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+
+@pytest.fixture(scope='function')
+def setup(request, get_webdriver):
+    driver = get_webdriver
+    url = 'https://demoqa.com/dynamic-properties'
+    if request.cls is not None:
+        request.cls.driver = driver
+    driver.get(url)
+    yield driver
+
+
+@pytest.fixture(scope='function', autouse=True)
+def start_end_test():
+    print('\nSTART TEST')
+    yield
+    print('\nEND TEST')
+
+
+@pytest.fixture()
+def get_id(setup):
+    element = setup.find_element(By.XPATH, '//p[contains(text(), "random")]')
+    yield element.get_attribute('id')
+
---
+++ selenium_tests_demoqa/dynamic-properties/test_page_dynamic-properties.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+def test_get_id(setup, get_id):
+    element_id = get_id
+    element_text = setup.find_element(By.ID, element_id).text
+    assert element_text == 'This text has random Id'
+
