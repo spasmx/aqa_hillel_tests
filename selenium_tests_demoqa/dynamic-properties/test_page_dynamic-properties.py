1513c4bc866fcb12a49874444a8435c5da73c9d2
Maksum <spasmx@DESKTOP-6E5H7JF>
2023-02-02 16:50:09 +0200

    HW18. checkboxes, radio-buttons, dynamic-properties

--- selenium_tests_demoqa/dynamic-properties/test_page_dynamic-properties.py
+++ selenium_tests_demoqa/dynamic-properties/test_page_dynamic-properties.py
@@ -1,4 +1,8 @@
+from typing import Any
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 def test_get_id(setup, get_id):
@@ -8,9 +12,26 @@ def test_get_id(setup, get_id):
 
 
 def test_wait_for_enable_element(setup):
-    pass
+    enable_button = setup.find_element(By.ID, 'enableAfter')
+    is_selected_enable_element(setup, 'enableAfter')
+    assert enable_button.is_enabled()
 
 
 def test_button_is_present(setup):
-    pass
+    setup.refresh()
+    is_visible(setup, 'visibleAfter')
+    enable_button = setup.find_element(By.ID, 'visibleAfter')
+    assert enable_button.is_displayed()
+
+
+def is_visible(setup, elem_id: str) -> Any:
+    return WebDriverWait(setup, 6).until(ec.visibility_of_element_located((By.ID, elem_id)))
+
+
+def is_selected_enable_element(setup, elem_id: str) -> Any:
+    return WebDriverWait(setup, 6).until(ec.element_to_be_clickable((By.ID, elem_id)))
+
+
+
+
 
